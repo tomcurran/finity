7ab93978926c17b2d144e59ce01ef0ca1b5c7de0
Tom Curran <tom@tomcurran.org>
2014-01-09 14:36:47 +0000

    self arcs implemented without normalisation of two control points of arc

--- Finity/src/org/tomcurran/finity/figure/connection/SelfArConnect.java
+++ Finity/src/org/tomcurran/finity/figure/connection/SelfArConnect.java
@@ -3,7 +3,7 @@ package org.tomcurran.finity.figure.connection;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
-import java.awt.geom.QuadCurve2D;
+import java.awt.geom.CubicCurve2D;
 
 import CH.ifa.draw.framework.Figure;
 
@@ -19,18 +19,56 @@ public class SelfArConnect implements ArcConnect {
 
 	@Override
 	public Point controlPoint() {
+		Point figureCentre = figureCentre();
+		Point connectionPoint = startEndPoint();
+		System.out.printf("connectionPoint=%s%n", connectionPoint);
+
+		Point v = new Point(connectionPoint.x - figureCentre.x, connectionPoint.y - figureCentre.y);
+		double vLen = Math.sqrt((v.x * v.x) + (v.y * v.y));
+
+		return new Point(
+				(int) (connectionPoint.x + ((v.x / vLen) * CONTROL_POINT_DISTANCE)),
+				(int) (connectionPoint.y + ((v.y / vLen) * CONTROL_POINT_DISTANCE)));
+	}
+
+	private Point figureCentre() {
 		Figure figure = connection.startFigure();
-		Point figureCentre = figure.center();
-		int h = figure.displayBox().height;
-		return new Point(figureCentre.x, figureCentre.y + (h / 2) + CONTROL_POINT_DISTANCE);
+		if (figure == null) {
+			figure = connection.endFigure();
+		}
+		return new Point(figure.center());
+	}
+
+	private Point startEndPoint() {
+		if (connection.start() != null) {
+			return connection.startPoint();
+		} else if (connection.end() != null) {
+			return connection.endPoint();
+		}
+		return null;
 	}
 
 	@Override
 	public void draw(Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
-		QuadCurve2D q = new QuadCurve2D.Float();
-		q.setCurve(connection.startPoint(), controlPoint(), connection.endPoint());
-		g2.draw(q);
+		CubicCurve2D c = new CubicCurve2D.Float();
+		Point control = controlPoint();
+		Point control1 = control1(control);
+		Point control2 = control2(control);
+		c.setCurve(connection.startPoint(), control1, control2, connection.endPoint());
+		g2.draw(c);
+	}
+
+	private Point control1(Point control) {
+		Point control1 = new Point(control);
+		control1.translate(CONTROL_POINT_DISTANCE, 0);
+		return control1;
+	}
+
+	private Point control2(Point control) {
+		Point control2 = new Point(control);
+		control2.translate(-CONTROL_POINT_DISTANCE, 0);
+		return control2;
 	}
 
 }
